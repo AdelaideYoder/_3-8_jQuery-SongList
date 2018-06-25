a111e12c3c2233382b1a9516eff339e49460e680
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-25 15:07:07 -0500

    used jQuery to print songs to the DOM

--- songs.js
+++ songs.js
@@ -1,20 +1,33 @@
 $(document).ready(function() {
-
     // Use jQuery to get a reference to `load-songs`
-
     // Use jQuery to get a reference to `song-list`
-
-
     /*
         Attach a click handler to the button with jQuery. When
         the button is clicked, use $.ajax() to load `songs.json`
-        from the file system
-    */
+        from the file system. Chain a `.then()` method to the ajax call 
+        */
 
+    $("#load-songs").click(() => {
+        $.ajax("songs.json")
+        .then(responseSongs => {
+            console.log(responseSongs)
+            responseSongs.songs
+            for (let i = 0; i < responseSongs.songs.length; i++) {
+                $("#song-list")
+                $("<h1>").text(responseSongs.songs[i].title)
+                .attr("class", "song__title")
+                .appendTo($("#song-list"));
 
-    /*
-        Chain a `.then()` method to the ajax call, and when
-        it is complete build a DOM component for each song with
+                $("<section>")
+                .text(`Performed by ${responseSongs.songs[i].artist} on the album ${responseSongs.songs[i].album}`)
+                .attr("class", "song__description")
+                .appendTo($("#song-list"));
+            }
+        })
+      })
+
+       
+       /* and when it is complete build a DOM component for each song with
         the following structure. Use the jQuery append() method
         to put an HTML representation of each song the DOM as a
         child component of the .
